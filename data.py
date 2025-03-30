883043150ef7d72c859ead6d1cbc3ece2aeab369
Arafat2342 <jashoresinger@gmail.com>
2025-03-30 14:27:57 +0600

    fixed

--- data.py
+++ data.py
@@ -1,11 +1,9 @@
 row = int(input('Enter the number of row of the right triangle: '))
-num = 1
 
 for i in range(1, row + 1):
     for j in range(1, row + 1):
         if j <= row - i:
             print(' ', end="  ")
         else:
-            print(num, end="  ")
-            num = num + 1
+            print('*', end="  ")
     print() 
